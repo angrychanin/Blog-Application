bc1ef5cbed9c14424017872a853def255a4bbae2
angrychanin <sun.chanin@gmail.com>
2020-05-05 16:12:59 +0700

    add mongoDB atlas

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/blog-application", {
-  // mongoose.connect(
-  //   "mongodb+srv://sun:<sungfdgmr123>@blogapp-i1zi8.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  // {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  process.env.MONGODB_ATLAS || "mongodb://127.0.0.1:27017/blog-application",
+  {
+    // mongoose.connect(
+    //   "mongodb+srv://sun:123@blogapp-i1zi8.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    // {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
