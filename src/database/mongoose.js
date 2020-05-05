a1f2c5b98587490377f35c610b023f511f2077c3
angrychanin <sun.chanin@gmail.com>
2020-05-05 19:17:14 +0700

    fix db route

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blog-application",
+  // "mongodb+srv://sun:123@blogapp-i1zi8.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  "mongodb://sun:sun123@ds247674.mlab.com:47674/heroku_stp7r7cl",
+  // || "mongodb://127.0.0.1:27017/blog-application"
   {
-    // mongoose.connect(
-    //   "mongodb+srv://sun:123@blogapp-i1zi8.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    // {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
