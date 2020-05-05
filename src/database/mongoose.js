10816eac05ffaae38bdf85078d88e0018d1ab93f
angrychanin <sun.chanin@gmail.com>
2020-05-05 18:38:42 +0700

    db

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_ATLAS || "mongodb://127.0.0.1:27017/blog-application",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/blog-application",
   {
     // mongoose.connect(
     //   "mongodb+srv://sun:123@blogapp-i1zi8.gcp.mongodb.net/test?retryWrites=true&w=majority",
